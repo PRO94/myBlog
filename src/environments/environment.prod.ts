a88628a6cfae6e35bdbbc3527a1fc147340cb298
Roman Prytulenko <roman.prytulenko@gmail.com>
2020-08-23 23:16:34 +0300

    Added auth, guards, error handling.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
+import {Environment} from './interface'
+
+export const environment: Environment = {
+  apiKey: 'AIzaSyCftFOJI26ZUs31Sab-2TGaTg7TEx4aT6s',
   production: true
-};
+}
